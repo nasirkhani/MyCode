c8dba4ba6d72f92fa085659bda2b763eda4c3d21
Nasir Khani <khani.nasir@gmail.com>
2024-06-29 18:31:32 +0330

    h1

---
+++ PycharmProjects/pythonProject1/Strings.py
@@ -0,0 +1,25 @@
+# s = "Hello How are you! Nasir is a good boy."
+#
+# c = input("Enter a character: ")
+# print(s.count(c))
+# ------------------------------------------------
+# #پیدا کردن آخرین کلمه ای که کاربر تایپ کرده
+# s = input("Enter a string: ")
+# s= s.rstrip()
+# i = s.rfind(" ")
+# print(s[i+1:])
+# ------------------------------------------------
+# str1=input("Enter a string1: ")
+# str2=input("Enter a string2: ")
+#
+# print(str2 in str1)
+# ------------------------------------------------
+
+# str1 = input("Enter a string1: ")
+# print(str1.replace(" ", "").replace("\t", ""))
+
+# ------------------------------------------------
+
+phone = input("Enter your phone number: ")
+print(phone.lstrip("0"))
+# ------------------------------------------------
---
+++ PycharmProjects/pythonProject1/g.py
@@ -0,0 +1,9 @@
+x = 5
+y = 7.65465434
+dic = {'x': 5, 'y': 8, 'z': 2.25889}
+print("x is {}\ny is {}\nz is {}".format(5, y, 5 + 7))
+print("x is {0}\ny is {1}\nz is {2}".format(5, y, 5 + 7))
+print("x is {2}\ny is {2}\nz is {2}".format(5, y, 5 + 7))
+print("x is {x}\ny is {y}\nz is {z}".format(**x))
+
+
