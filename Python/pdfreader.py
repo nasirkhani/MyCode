986c3459a9a03dc064ad8a43e8c0bd8a2ce84bfb
Nasir Khani <khani.nasir@gmail.com>
2024-07-12 18:06:07 +0330

    Delete Python directory

--- Python/pdfreader.py
+++
@@ -1,6 +0,0 @@
-import PyPDF2
-with open('1.pdf', 'rb') as pdf:
-    read_pdf = pypdf2.pdffilereader(pdf)
-    pdf_page=read_pdf.getpage(1)
-    pdf_content=pdf_page.extractText()
-    print(pdf_content)
