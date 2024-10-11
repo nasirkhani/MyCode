475ab63e51ca98315b69e8a89bdc93fc429a4dad
Nasir Khani <khani.nasir@gmail.com>
2024-10-11 20:07:12 +0330

    Deleting unnecessary things.

--- datatime/T.py
+++
@@ -1,8 +0,0 @@
-from khayyam import JalaliDatetime, JalaliDate
-
-print(JalaliDatetime.now())
-print(JalaliDate.today())
-print(JalaliDate.today().strftime('%A %d %B %Y'))
-print(JalaliDate(1403, 4, 26).todate())
-
-
--- oop/T1.py
+++
@@ -1,6 +0,0 @@
-import Classes.MathOperation as riazi
-
-my_math = riazi.Math(10, 20)
-
-print(my_math.addition())
-print(my_math.subtraction())
