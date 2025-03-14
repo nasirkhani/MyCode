649c5dfa483da8aaebcb24cd6670c48cff5351b6
Nasir Khani <khani.nasir@gmail.com>
2025-03-14 19:49:16 +0330

    a

--- PycharmProjects/1/pythonProject/Dictionary.py
+++
@@ -1,7 +0,0 @@
-Country_Capitals = {
-    "Iran": "Tehran",
-    "Germany": "Berlin",
-    "Turkey": "Istanbul"
-}
-print(Country_Capitals)
-
--- PycharmProjects/1/pythonProject/number.py
+++
@@ -1,5 +0,0 @@
-#x = 0b011001
-#print(x)
-from decimal import Decimal as d
-f = 0.6 + 0.3
-print(d(f))
